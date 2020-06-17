c3e7a9d1c53abb7b0b14e02ff13f39f2bf9faeb8
GermainPereira <pereira.germain@outlook.com>
2020-06-16 22:01:24 -0300

    Passed tests

--- src/app.js
+++ src/app.js
@@ -1,33 +1,103 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
-
+// init express
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
+// repo for RAM storge
 const repositories = [];
 
+// List
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
+// Create
 app.post("/repositories", (request, response) => {
-  // TODO
+  // Create new id and likes
+  const id = uuid();
+  const likes = 0;
+
+  // Extract objs from request
+  const { title, url, techs } = request.body;
+
+  // Create and store new Repo
+  const newRepository = { id, title, url, techs, likes };
+  repositories.push(newRepository);
+
+  return response.json(newRepository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
+// Add like
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  // Get request id
+  const { id } = request.params;
+
+  // Search for matching ID within the Repositories
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  // Return Bad Request if doesn't exist
+  if (repositoryId < 0) {
+    return response.status(400).json({ error: "Bad Request" });
+  }
+
+  // Add one more like to the count
+  repositories[repositoryId]["likes"]++;
+
+  return response.status(200).json(repositories[repositoryId]);
+});
+
+// Update
+app.put("/repositories/:id", (request, response) => {
+  // Extract objs from request
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  // Search for matching ID within the Repositories
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id == id
+  );
+
+  // Return Bad Request, if doesn't exist
+  if (repositoryId < 0) {
+    return response.status(400).json({ error: "Bad Request" });
+  }
+
+  // Repeat the current repository's data, actualizing the new ones
+  repositories[repositoryId] = {
+    ...repositories[repositoryId],
+    title,
+    url,
+    techs,
+  };
+
+  return response.status(200).json(repositories[repositoryId]);
+});
+
+// Delete
+app.delete("/repositories/:id", (request, response) => {
+  // Extract id from Request
+  const { id } = request.params;
+
+  // Search for matching ID within the Repositories
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id == id
+  );
+
+  // Return Bad Request if doesn't exist
+  if (repositoryId < 0) {
+    return response.status(400).json({ error: "Bad Request" });
+  }
+
+  // Remoove the repository by object's id
+  repositories.splice(repositoryId, 1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
